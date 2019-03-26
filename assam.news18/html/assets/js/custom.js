01851d197b45473afae30d3b0b75ca878039ed32
Avinash Chand <avinash.c.pal@gmail.com>
2019-03-26 18:56:23 +0530

    addede

---
+++ assam.news18/html/assets/js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.ts_nw_list:first-of-type').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        arrows:false
+    });
+
+
+    // $('.carousel_list').slick({
+    //     dots: true,
+    //     infinite: false,
+    //     speed: 500,
+    //     arrows:true,
+    //     slideToShow:3,
+    //     slidToScroll:1
+    // });
+    // $('#carousel_cont .carousel_list').slick({
+    //   infinite: true,
+    //   slidesToShow: 3,
+    //   slidesToScroll: 1,
+    //   infinite:false
+    // });
+
+
+});
