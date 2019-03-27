7e7948524b9c4be578e9aa395a9ee3cbc8bba04e
avinash-git-code <avinash.c.pal@gmail.com>
2019-03-28 00:27:44 +0530

    slick slider

--- assam.news18/html/assets/js/custom.js
+++ assam.news18/html/assets/js/custom.js
@@ -7,10 +7,33 @@ $(document).ready(function(){
     });
 
     $('.carousel_list').slick({
-         infinite: false,
+         infinite: true,
          slidesToShow: 3,
          slidesToScroll: 1,
          prevArrow: '<button type="button" class="snp cust-prev"><span></span></button>',
          nextArrow: '<button type="button" class="snp cust-next"><span></span></button>'
       });
+
+      // $('.slider_stories_cont ul').slick({
+      //      infinite: true,
+      //      slidesToShow: 1,
+      //      slidesToScroll: 1
+      //   });
+
+      $(function() {
+    $('.slider_stories_cont ul').on('init', function(event, slick) {
+      $(this).append('<div class="slick-counter"><span class="current"></span> of <span class="total"></span></div>');
+      $('.current').text(slick.currentSlide + 1);
+      $('.total').text(slick.slideCount);
+    })
+    .slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      infinite: true,
+      arrows: true
+    })
+    .on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+      $('.current').text(nextSlide + 1);
+    });
+  });
 });
