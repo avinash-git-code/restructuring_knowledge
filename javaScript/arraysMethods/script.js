e48104c7da57e816212337c10f2df31981a2fe18
Avinash Chand <avinash.c.pal@gmail.com>
2019-10-29 16:36:51 +0530

    method of array

--- javaScript/arraysMethods/script.js
+++ javaScript/arraysMethods/script.js
@@ -1,8 +1,46 @@
 // forEach
-// map
-// filter
-// reduce
-// some
-// every
-// find
-// findeIndex
+// map, it's return a new array.
+// filter, it's return a spacific new filtered array.
+// reduce, it's return a value after perform action on all array values.
+// some, return boolean value if any condition is true into the following array that will return a true value.
+// every, it's smiler to some but it's check all condition is ture or not.
+// find, it's check value is exist or not into given array if value if find into the array return that value .
+// findeIndex, it's smiler to find but it's provide the index of that array.
+
+let arr = [1, 2, 3, 4, 5, 6]; // 21
+let total = 0;
+
+// arr.forEach(function(item, index) {
+//   total += item;
+// });
+
+// let x = arr.map(function(item, index) {
+//   return (total += item);
+// });
+
+// let x = arr.filter(function(item, index) {
+//   return item > 4;
+// });
+
+// let x = arr.reduce(function(item, index) {
+//   return item + index;
+// }, 0);
+
+// let x = arr.some(function(item, index) {
+//   return item > 1;
+// });
+
+// let x = arr.every(function(item, index) {
+//   return item > 0;
+// });
+
+// let x = arr.find(function(item, index) {
+//   return item == 4;
+// });
+
+// let x = arr.findIndex(function(item, index) {
+//   return item == 6;
+// });
+
+console.log(total);
+console.log(x);
